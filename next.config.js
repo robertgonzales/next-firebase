757ba5d8bc4d7933534d4dff92b8f81ec56db2b5
Robert Gonzales <robert@robertgonzal.es>
2019-03-28 17:16:08 -0400

    initial commit

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const firebase = require("firebase")
+
+firebase.initializeApp({
+  apiKey: "AIza....", // Auth / General Use
+  authDomain: "YOUR_APP.firebaseapp.com", // Auth with popup/redirect
+  databaseURL: "https://YOUR_APP.firebaseio.com", // Realtime Database
+  storageBucket: "YOUR_APP.appspot.com", // Storage
+  messagingSenderId: "123456789", // Cloud Messaging
+})
+
+const db = firebase.database()
+
+module.exports = {
+  useFileSystemPublicRoutes: true,
+}
